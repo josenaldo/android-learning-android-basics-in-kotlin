53d7602734b5065298a94545a8ddbfebcf81c780
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-02 07:15:04 -0300

    Inicio do projeto Dice Roller

---
+++ unit1-kotlin-basics/pathway-2-to-4/dice-roller/app/src/main/java/br/com/vudureverso/diceroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.vudureverso.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
