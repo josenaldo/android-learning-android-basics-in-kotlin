3863e4e73c994d094398739275f9ffa87dd47172
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-02 08:13:36 -0300

    Dado foi adicionado. Agora, ao clicar no botão, um dado é jogado e o resultado mostrado na tela!

--- unit1-kotlin-basics/pathway-2-to-4/dice-roller/app/src/main/java/br/com/vudureverso/diceroller/MainActivity.kt
+++ unit1-kotlin-basics/pathway-2-to-4/dice-roller/app/src/main/java/br/com/vudureverso/diceroller/MainActivity.kt
@@ -14,9 +14,24 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
 
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.resultTextView)
+            rollDice()
 
-            resultTextView.text = "6"
+
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.resultTextView)
+        resultTextView.text = diceRoll.toString()
+
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
