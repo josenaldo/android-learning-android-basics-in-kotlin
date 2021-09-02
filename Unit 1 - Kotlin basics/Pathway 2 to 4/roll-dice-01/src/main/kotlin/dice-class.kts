5e7ba886785416011f1460e92b62309f9f9458f2
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-02 06:16:54 -0300

    4. Mostrar o valor resultante do dado

---
+++ Unit 1 - Kotlin basics/Pathway 2 to 4/roll-dice-01/src/main/kotlin/dice-class.kts
@@ -0,0 +1,16 @@
+fun main(){
+    val myFirstDice = Dice()
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.sides} sided dice rolled $diceRoll")
+}
+
+class Dice {
+    var sides = 6
+
+    fun roll(): Int {
+        return (1..6).random()
+    }
+}
+
+
+main()
