f1daa8bc7e54aef78b620bfb5224fdceebf8668e
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-02 08:05:44 -0300

    Botão adicionado Texto configurado pra ser modificado ao clicar o botão

--- unit1-kotlin-basics/pathway-2-to-4/dice-roller/app/src/main/java/br/com/vudureverso/diceroller/MainActivity.kt
+++ unit1-kotlin-basics/pathway-2-to-4/dice-roller/app/src/main/java/br/com/vudureverso/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@ package br.com.vudureverso.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.resultTextView)
+
+            resultTextView.text = "6"
+        }
     }
 }
